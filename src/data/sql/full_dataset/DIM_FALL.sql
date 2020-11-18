d55b2bf108f77b732ddee7968d288758b5278a0e
Benjamin Ellenberger <benjamin.ellenberger@insel.ch>
2020-11-18 14:09:46 +0100

    Add DIM_FALL cases query

---
+++ src/data/sql/full_dataset/DIM_FALL.sql
@@ -0,0 +1,11 @@
+SELECT
+    [PATIENTID]         = dim_fall_patient_bk
+    ,[CASEID]           = dim_fall_bk
+    ,[CASETYP]          = dim_fall_fallart_cd
+    ,[CASESTATUS]       = dim_fall_status
+    ,[FALAR]            = dim_fall_fallart
+    ,[BEGDT]            = dim_fall_start_datum
+    ,[ENDDT]            = dim_fall_ende_datum
+    ,[PATIENTTYP]       = dim_patient_typ
+    ,[PATIENTSTATUS]    = dim_patient_status
+FROM [Atelier_DataScience].[atl].[dim_fall]
