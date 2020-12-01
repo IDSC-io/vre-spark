7a003e0545fc2720bf0d921c005b4e18ce6b55a5
benelot <be.ellenberger@gmail.com>
2020-12-01 18:49:13 +0100

    Glob metric csvs and write/append them to the [temp].[metric_{metric_name}] database table

--- src/postprocessing/export_metrics_to_db.py
+++ src/postprocessing/export_metrics_to_db.py
@@ -18,6 +18,8 @@ import os
 import pandas as pd
 from sqlalchemy import create_engine
 import pyodbc
+from pathlib import Path
+import re
 
 
 def get_mssql_engine(driver=os.getenv("DRIVER"),
@@ -29,6 +31,24 @@ def get_mssql_engine(driver=os.getenv("DRIVER"),
     return engine
 
 
+def get_all_timestamped_metrics(metrics_folder="./data/processed/metrics/"):
+    # get all csvs in folder
+    file_paths = Path(metrics_folder).glob("*.csv")
+
+    metric_paths_list = []
+    # extract timestamp from csv prefix
+    pattern = "^([0-9]+)_(.+)\\.csv$"
+    for file_path in file_paths:
+        match = re.search(pattern, Path(file_path).name)
+        if match is not None:
+            # if extraction was successful, read metric df and compose tuple
+            timestamp, metric_name = match.groups()
+            metric_df = pd.read_csv(file_path, encoding="iso-8859-1")
+            metric_paths_list.append((timestamp, metric_name, metric_df))
+
+    return metric_paths_list
+
+
 # @click.command()
 # @click.argument('input_filepath', type=click.Path(exists=True))
 # @click.argument('output_filepath', type=click.Path())
@@ -38,19 +58,16 @@ def create_append_df_to_sql():
                         datefmt='%d.%m.%Y %H:%M:%S')
     #####################################
 
-    now_str = datetime.now().strftime("%Y%m%d%H%M%S")
-
-    df = pd.read_csv("./data/raw/model_data/ANTIBIOTICS_INTAKE.csv", encoding="iso-8859-1")
-
     engine = get_mssql_engine(driver="SQL+Server",
                                              host="WSISRZ0089\SISP22T",
                                              db="Atelier_DataScience",
                                              user="Atelier_DataScience_reader",
                                              password="A7D32FXXA2F&=K5QM40")
 
-
-    table_name = "example_table"
-    df.to_sql(table_name, engine, schema="temp", if_exists='append', chunksize=1000)
+    for (timestamp, metric_name, metric_df) in get_all_timestamped_metrics():
+         metric_df["Timstamp"] = datetime.strptime(timestamp, "%Y%m%d%H%M%S")
+         table_name = f"metric_{metric_name}"
+         metric_df.to_sql(table_name, engine, schema="temp", if_exists='append', chunksize=1000)
 
     logging.info("Data exported successfully!")
 
